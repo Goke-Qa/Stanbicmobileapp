0e7ec67584a5277b1cef1fa91746b8f8312b41bd
Edet <Uwakmfon.Edet@stanbicibtc.com>
2021-02-26 16:24:56 +0100

    Framework Structure

--- StanbicIBTC_SingleSignOnProject/src/test/java/mobileScreens/CreateOnePassScreens.java
+++ StanbicIBTC_SingleSignOnProject/src/test/java/mobileScreens/CreateOnePassScreens.java
@@ -16,17 +16,60 @@ public class CreateOnePassScreens extends DriverFactory{
 	By btn_profileInsurance = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[5]/android.view.ViewGroup");
 	By btn_profileStocks = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[6]/android.view.ViewGroup");
 
+	//Elements to Login to Internet Banking
+	By txt_internetBankingID =  By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.widget.EditText");
+	By txt_internetBankingPassword =  By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.EditText");
+	By btn_internetBankingLogin =  By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[6]");
 
+	//Elements for OTP validation 
+	By txt_tokenNum1 = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.TextView");
+	By txt_tokenNum2 = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.TextView");
+	By txt_tokenNum3 = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup[3]/android.view.ViewGroup/android.widget.TextView");
+	By txt_tokenNum4 = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.TextView");
+	By txt_tokenNum5 = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.TextView");
+
+	By btn_createID = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]");
+	
 	public void clickCreateOnepassButton() {
 		waitVar.until(ExpectedConditions.visibilityOfElementLocated(btn_createOnePass));
 		driver.findElement(btn_createOnePass).click();
 		waitVar.until(ExpectedConditions.visibilityOfElementLocated(btn_agreeToTerms));
 		driver.findElement(btn_agreeToTerms).click();
 	}
-	
+
 	public void selectLoginModuleMyBank() {
 		waitVar.until(ExpectedConditions.visibilityOfElementLocated(btn_profileMyBank));
 		driver.findElement(btn_profileMyBank).click();
 	}
 
+	public void loginMyBank(String internetBanking, String internetBankingPassword) {
+		waitVar.until(ExpectedConditions.visibilityOfElementLocated(btn_internetBankingLogin));
+		driver.findElement(txt_internetBankingID).sendKeys(internetBanking);
+		driver.findElement(txt_internetBankingPassword).sendKeys(internetBankingPassword);
+		driver.findElement(btn_internetBankingLogin).click();
+	}
+
+	public void checkOTPsent() {
+		waitVar.until(ExpectedConditions.visibilityOfElementLocated(txt_tokenNum1));
+		if(driver.findElement(txt_tokenNum1).isDisplayed()) {
+			System.out.println("User has been sent OTP");
+		}
+		else {
+			System.out.println("User could not be logged in");
+		}
+	}
+	
+	public void enterOTP() {
+		waitVar.until(ExpectedConditions.visibilityOfElementLocated(txt_tokenNum1));
+		driver.findElement(txt_tokenNum1).click();
+		driver.findElement(txt_tokenNum2).click();
+		driver.findElement(txt_tokenNum3).click();
+		driver.findElement(txt_tokenNum4).click();
+		driver.findElement(txt_tokenNum5).click();
+	}
+	
+	public void createOnePassID(String OnePassID) {
+		waitVar.until(ExpectedConditions.visibilityOfElementLocated(btn_createID));
+		
+	}
 }
--- StanbicIBTC_SingleSignOnProject/src/test/resources/features/Create_OnePass_Account.feature
+++ StanbicIBTC_SingleSignOnProject/src/test/resources/features/Create_OnePass_Account.feature
@@ -4,4 +4,10 @@ Scenario: Create OnePass Account with valid details
 
 When user clicks to create OnePass Account and Agrees to terms
 Then user is required to select a login module
-When users enter module credentials and logs in
+When user enters valid module credentials and logs in
+Then an OTP should be sent
+When user validates the OTP
+Then user should create OnePass ID 
+And Create a password
+When user selects a security question and response
+Then OnePass Account should be created successfully
